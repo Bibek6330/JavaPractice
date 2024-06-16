454c15449de6bbbb7468d4e04ba37f663b5ca923
Bibek6330 <bibek6330@gmail.com>
2024-06-16 23:23:48 +0530

    adding files

---
+++ src/Practice11.java
@@ -0,0 +1,35 @@
+public class Practice11 {
+    class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+        return 0;
+    }
+
+    public int getMin() {
+
+        return 0;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+}
